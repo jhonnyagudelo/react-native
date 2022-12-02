f0bc80e3cda3d94694dea5d7946e59b784b93e52
jhonny stiven agudelo tenorio <jhonny.agudelo3120@gmail.com>
2022-12-01 21:51:03 -0500

    flexbox

--- 02-MiPrimeraApp/src/screens/FlexScreen.tsx
+++ 02-MiPrimeraApp/src/screens/FlexScreen.tsx
@@ -3,12 +3,38 @@ import React from 'react';
 
 const FlexScreen = () => {
   return (
-    <View>
-      <Text>FlexScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.caja1}>Caja 1</Text>
+      <Text style={styles.caja2}>Caja 2</Text>
+      <Text style={styles.caja3}>Caja 3</Text>
     </View>
   );
 };
 
 export { FlexScreen };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#28C4D9',
+    justifyContent: 'center',
+  },
+  caja1: {
+    // flex: 4,
+    borderWidth: 2,
+    borderColor: 'white',
+    fontSize: 30,
+  },
+  caja2: {
+    // flex: 4,
+    borderWidth: 2,
+    borderColor: 'white',
+    fontSize: 30,
+  },
+  caja3: {
+    // flex: 2,
+    borderWidth: 2,
+    borderColor: 'white',
+    fontSize: 30,
+  },
+});
